22bc2cc9f97904e6e868056af58c007aa05ef95a
ajay-makvana <ajaymakvana2001@gmail.com>
2021-05-18 09:40:25 +0530

    DistrictWise data for every State created

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
 
 def home(request):
     return render(request,'home/index.html');
+
+def DistrictWise(request,state):
+    return render(request,'home/DistrictWise.html',{'state':state});
