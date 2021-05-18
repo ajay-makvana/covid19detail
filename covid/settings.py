fa6cfb0448cf61cc8819109b5cb0a0eb9f3d885f
ajay-makvana <ajaymakvana2001@gmail.com>
2021-05-18 13:17:27 +0530

    Timezone and Some other changes

--- covid/settings.py
+++ covid/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^kvg%7ofo=b&e78xrk0vvr*q%j@5!z2ban&44o9cf22z!-nopb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTD = []
 ALLOWED_HOSTS = ['covid19detail.herokuapp.com']
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
