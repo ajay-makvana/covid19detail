ee124f5f578887a120075438546b318c908b51f3
ajay-makvana <ajaymakvana2001@gmail.com>
2021-05-21 13:59:17 +0530

    Revert "Update settings.py"
    This reverts commit cf7e11352c9795a814cbe7c40376957f40364b07.

--- covid/settings.py
+++ covid/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-^kvg%7ofo=b&e78xrk0vvr*q%j@5!z2ban&44o9cf22z!-nopb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
